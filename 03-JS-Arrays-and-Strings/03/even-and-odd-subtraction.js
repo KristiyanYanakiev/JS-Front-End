20cad1230d05ab2ba93def310d93e92334f046e2
KristiyanYanakiev <yankris_92@yahoo.com>
2025-05-23 12:56:52 +0300

    more solutions

--- 03-JS-Arrays-and-Strings/03/even-and-odd-subtraction.js
+++ 03-JS-Arrays-and-Strings/03/even-and-odd-subtraction.js
@@ -1,18 +1,34 @@
+// function solve(arr) {
+//     let evenSum = 0
+//     let oddSum = 0
+
+//     for (let n of arr) {
+//         if (n % 2 ==0) {
+//             evenSum += n 
+//         }
+
+//         else {
+//             oddSum += n
+//         }
+//     }
+
+//     console.log((evenSum - oddSum))
+// }
+
 function solve(arr) {
-    let evenSum = 0
-    let oddSum = 0
+    let eS = 0
+    let oS = 0
 
-    for (let n of arr) {
-        if (n % 2 ==0) {
-            evenSum += n 
+    arr.forEach(element => {
+        if (element % 2 == 0) {
+            eS += element
         }
-
         else {
-            oddSum += n
+            oS += element
         }
-    }
+    });
 
-    console.log((evenSum - oddSum))
+    console.log(eS - oS)
 }
 
 solve([1,2,3,4,5,6])
