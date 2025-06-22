71e4a2cc82c6f28540f22a5645770d16c7c90381
KristiyanYanakiev <yankris_92@yahoo.com>
2025-06-22 15:48:41 +0300

    more solutions

--- 09-JS-DOM-Introduction-Lab/05.Colorize Table/colorize-table.js
+++ 09-JS-DOM-Introduction-Lab/05.Colorize Table/colorize-table.js
@@ -1,5 +1,5 @@
 function colorize() {
-    const evenRows = document.querySelectorAll("table tbody tr:nth-child(odd)")
+    const evenRows = document.querySelectorAll("table tbody tr:nth-child(even)")
     
     evenRows.forEach(element => {
         element.style.backgroundColor = 'teal'
