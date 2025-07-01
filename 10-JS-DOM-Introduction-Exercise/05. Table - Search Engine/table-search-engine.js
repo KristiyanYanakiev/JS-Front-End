4973b0eb4e63f7f0cad28c400fbe31e39555a689
KristiyanYanakiev <yankris_92@yahoo.com>
2025-07-01 17:20:53 +0300

    more solutions

--- 10-JS-DOM-Introduction-Exercise/05. Table - Search Engine/table-search-engine.js
+++ 10-JS-DOM-Introduction-Exercise/05. Table - Search Engine/table-search-engine.js
@@ -1,3 +1,19 @@
 function solve() {
-   //TODO
+   const rowsToSearcheIn = document.querySelectorAll("table.container tbody tr")
+
+   const searchedValue = document.querySelector("#searchField").value.toLowerCase().trim()
+
+
+   
+   if (searchedValue === "") return
+   rowsToSearcheIn.forEach(td => {
+      
+      td.classList.remove("select")
+      if (td.textContent.toLowerCase().includes(searchedValue)) {
+         td.classList.add("select")
+         
+      }
+   })
+
+
 }
--- 10-JS-DOM-Introduction-Exercise/06. Format the Text/format-the-text.js
+++ 10-JS-DOM-Introduction-Exercise/06. Format the Text/format-the-text.js
@@ -1,3 +1,27 @@
 function solve() {
-  // TODO
-}
+    const textToFormat = document.querySelector("#input").value
+    
+    const sentences = textToFormat.split(". ")
+
+    const divElement = document.querySelector("#output")
+
+    const sentencePerParagraph = 3
+
+    const numberOfParagraps = Math.ceil(sentences.length / sentencePerParagraph)
+
+    let output = "";
+
+    for (let i = 0; i < numberOfParagraps; i++) {
+
+      const start = i * sentencePerParagraph;
+      const end = start + sentencePerParagraph
+
+      output += "<p>"
+      output += sentences.slice(start, end).join(". ")
+      output += "</p>"
+      
+
+    }
+
+    divElement.innerHTML = output
+  }
